078f7bceac2794816481bc5652a7880efeb778b4
mohamedmostafarizk <mohamed.most1311@gmail.com>
2025-07-19 07:33:06 +0300

    create get started page

---
+++ lib/Presentation/views/getStarted.dart
@@ -0,0 +1,59 @@
+// ignore_for_file: file_names
+
+import 'package:flutter/material.dart';
+
+class GetStarted extends StatelessWidget {
+  const GetStarted({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SizedBox(
+        width: double.infinity,
+        child: Padding(
+          padding: const EdgeInsets.only(top: 150),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Image.asset('asset/images/0_2.png'),
+              SizedBox(height: 50),
+              Text(
+                'Got yourself a reminder',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),
+              ),
+              SizedBox(height: 10),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8),
+                child: Text(
+                  'You no longer need to worry about forgetting when to take your medicine, we are here to remind you to take your medicine on time.',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 16, color: Colors.grey[700]),
+                ),
+              ),
+              SizedBox(height: 250),
+              Container(
+                margin: const EdgeInsets.symmetric(horizontal: 12),
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(16),
+                  color: Colors.blue,
+                ),
+                child: TextButton(
+                  onPressed: () {},
+                  child: const Text(
+                    'Get Started',
+                    style: TextStyle(color: Colors.white, fontSize: 18),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/Presentation/views/splashView.dart
+++ lib/Presentation/views/splashView.dart
@@ -1,7 +1,7 @@
 // ignore_for_file: file_names, use_build_context_synchronously
 
 import 'package:flutter/material.dart';
-import 'package:medcineapp/Presentation/views/homeView.dart';
+import 'package:medcineapp/Presentation/views/getStarted.dart';
 
 class SplashView extends StatefulWidget {
   const SplashView({super.key});
@@ -19,7 +19,7 @@ class _SplashViewState extends State<SplashView> {
     Future.delayed(const Duration(seconds: 2), () {
       Navigator.push(
         context,
-        MaterialPageRoute(builder: (context) => const Homeview()),
+        MaterialPageRoute(builder: (context) => const GetStarted()),
       );
     });
   }
