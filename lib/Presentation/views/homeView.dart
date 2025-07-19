47c2672a8e471fd0fe3da806ed445697d0df951a
mohamedmostafarizk <mohamed.most1311@gmail.com>
2025-07-19 07:12:29 +0300

    wait 2 sec until navigate

---
+++ lib/Presentation/views/homeView.dart
@@ -0,0 +1,12 @@
+// ignore_for_file: file_names
+
+import 'package:flutter/material.dart';
+
+class Homeview extends StatelessWidget {
+  const Homeview({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(body: Center(child: Text('Home View')));
+  }
+}
--- lib/Presentation/views/splashView.dart
+++ lib/Presentation/views/splashView.dart
@@ -1,9 +1,28 @@
-// ignore_for_file: file_names
+// ignore_for_file: file_names, use_build_context_synchronously
 
 import 'package:flutter/material.dart';
+import 'package:medcineapp/Presentation/views/homeView.dart';
 
-class SplashView extends StatelessWidget {
+class SplashView extends StatefulWidget {
   const SplashView({super.key});
+
+  @override
+  State<SplashView> createState() => _SplashViewState();
+}
+
+class _SplashViewState extends State<SplashView> {
+  @override
+  void initState() {
+    super.initState();
+
+    // Wait 2 seconds and navigate to HomeView
+    Future.delayed(const Duration(seconds: 2), () {
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => const Homeview()),
+      );
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
