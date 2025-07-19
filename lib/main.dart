a5ecf0098fc89ab3132db609e1584966f57aff74
mohamedmostafarizk <mohamed.most1311@gmail.com>
2025-07-19 06:52:53 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MedcineApp());
+}
+
+class MedcineApp extends StatelessWidget {
+  const MedcineApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(home: Scaffold());
+  }
+}
