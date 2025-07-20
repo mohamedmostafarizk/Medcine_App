6e461bee5e71cb2065ef2f8e09556686a58e9276
mohamedmostafarizk <mohamed.most1311@gmail.com>
2025-07-20 06:05:03 +0300

    edit build gradle in andriod folder

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,24 +1,22 @@
 buildscript {
-  dependencies {
-    // ... other dependencies
-    classpath 'com.google.gms:google-services:4.4.3'
-  }
-}
-
-allprojects {
+    dependencies {
+        // ... other dependencies
+        classpath("com.google.gms:google-services:4.4.3")
+    }
     repositories {
         google()
         mavenCentral()
     }
 }
 
-val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
-rootProject.layout.buildDirectory.value(newBuildDir)
+val newBuildDir = rootProject.layout.buildDirectory.dir("../../build").get()
+rootProject.layout.buildDirectory.set(newBuildDir)
 
 subprojects {
-    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
-    project.layout.buildDirectory.value(newSubprojectBuildDir)
+    val newSubprojectBuildDir = newBuildDir.dir(project.name)
+    project.layout.buildDirectory.set(newSubprojectBuildDir)
 }
+
 subprojects {
     project.evaluationDependsOn(":app")
 }
