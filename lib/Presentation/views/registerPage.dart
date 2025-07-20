2cc75b7168268a4b52dc71ec83b8a9f0331639d2
mohamedmostafarizk <mohamed.most1311@gmail.com>
2025-07-20 10:37:08 +0300

    add scroll to register page

--- lib/Presentation/views/registerPage.dart
+++ lib/Presentation/views/registerPage.dart
@@ -19,242 +19,245 @@ class _RegisterpageState extends State<Registerpage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SizedBox.expand(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const Padding(
-              padding: EdgeInsets.only(top: 70, left: 85),
-              child: Text(
-                'Create An Account',
-                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Padding(
+                padding: EdgeInsets.only(top: 70, left: 85),
+                child: Text(
+                  'Create An Account',
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                ),
               ),
-            ),
-            const SizedBox(height: 50),
-            const Padding(
-              padding: EdgeInsets.only(left: 15),
-              child: Text(
-                'Name',
-                style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+              const SizedBox(height: 50),
+              const Padding(
+                padding: EdgeInsets.only(left: 15),
+                child: Text(
+                  'Name',
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+                ),
               ),
-            ),
-            const SizedBox(height: 5),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 12),
-              child: SizedBox(
-                height: 50,
-                child: TextField(
-                  decoration: InputDecoration(
-                    filled: true,
-                    fillColor: const Color(0xFFF2F8FF),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(16),
+              const SizedBox(height: 5),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 12),
+                child: SizedBox(
+                  height: 50,
+                  child: TextField(
+                    decoration: InputDecoration(
+                      filled: true,
+                      fillColor: const Color(0xFFF2F8FF),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(16),
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
-            const SizedBox(height: 20),
-            const Padding(
-              padding: EdgeInsets.only(left: 15),
-              child: Text(
-                'phone number',
-                style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+              const SizedBox(height: 20),
+              const Padding(
+                padding: EdgeInsets.only(left: 15),
+                child: Text(
+                  'phone number',
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+                ),
               ),
-            ),
-            const SizedBox(height: 5),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 12),
-              child: SizedBox(
-                height: 50,
-                child: TextField(
-                  keyboardType: TextInputType.number, // Shows numeric keyboard
-                  inputFormatters: [
-                    FilteringTextInputFormatter
-                        .digitsOnly, // Allows only digits
-                  ],
-                  decoration: InputDecoration(
-                    filled: true,
-                    fillColor: const Color(0xFFF2F8FF),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(16),
+              const SizedBox(height: 5),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 12),
+                child: SizedBox(
+                  height: 50,
+                  child: TextField(
+                    keyboardType:
+                        TextInputType.number, // Shows numeric keyboard
+                    inputFormatters: [
+                      FilteringTextInputFormatter
+                          .digitsOnly, // Allows only digits
+                    ],
+                    decoration: InputDecoration(
+                      filled: true,
+                      fillColor: const Color(0xFFF2F8FF),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(16),
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
 
-            const SizedBox(height: 20),
-            const Padding(
-              padding: EdgeInsets.only(left: 15),
-              child: Text(
-                'Email',
-                style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+              const SizedBox(height: 20),
+              const Padding(
+                padding: EdgeInsets.only(left: 15),
+                child: Text(
+                  'Email',
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+                ),
               ),
-            ),
-            const SizedBox(height: 5),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 12),
-              child: SizedBox(
-                height: 50,
-                child: TextField(
-                  decoration: InputDecoration(
-                    filled: true,
-                    fillColor: const Color(0xFFF2F8FF),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(16),
+              const SizedBox(height: 5),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 12),
+                child: SizedBox(
+                  height: 50,
+                  child: TextField(
+                    decoration: InputDecoration(
+                      filled: true,
+                      fillColor: const Color(0xFFF2F8FF),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(16),
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
-            const SizedBox(height: 20),
-            const Padding(
-              padding: EdgeInsets.only(left: 15),
-              child: Text(
-                'Password',
-                style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+              const SizedBox(height: 20),
+              const Padding(
+                padding: EdgeInsets.only(left: 15),
+                child: Text(
+                  'Password',
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+                ),
               ),
-            ),
-            const SizedBox(height: 5),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 12),
-              child: SizedBox(
-                height: 50,
-                child: TextField(
-                  obscureText: _obscurePassword,
-                  decoration: InputDecoration(
-                    filled: true,
-                    fillColor: const Color(0xFFF2F8FF),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(16),
+              const SizedBox(height: 5),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 12),
+                child: SizedBox(
+                  height: 50,
+                  child: TextField(
+                    obscureText: _obscurePassword,
+                    decoration: InputDecoration(
+                      filled: true,
+                      fillColor: const Color(0xFFF2F8FF),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(16),
+                      ),
+                      suffixIcon: IconButton(
+                        icon: Icon(
+                          _obscurePassword
+                              ? Icons.visibility_off
+                              : Icons.visibility,
+                        ),
+                        onPressed: () {
+                          setState(() {
+                            _obscurePassword = !_obscurePassword;
+                          });
+                        },
+                      ),
                     ),
-                    suffixIcon: IconButton(
-                      icon: Icon(
-                        _obscurePassword
-                            ? Icons.visibility_off
-                            : Icons.visibility,
-                      ),
-                      onPressed: () {
-                        setState(() {
-                          _obscurePassword = !_obscurePassword;
-                        });
-                      },
-                    ),
                   ),
                 ),
               ),
-            ),
-            const SizedBox(height: 40),
-            Padding(
-              padding: const EdgeInsets.only(left: 20),
-              child: ElevatedButton(
-                onPressed: () {
-                  // Handle login action
-                },
-                style: ElevatedButton.styleFrom(
-                  fixedSize: const Size(340, 50),
-                  backgroundColor: const Color(0xFF4A90E2),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(24),
-                  ),
-                ),
-                child: const Text(
-                  'Register',
-                  style: TextStyle(fontSize: 16, color: Colors.white),
-                ),
-              ),
-            ),
-            const SizedBox(height: 20),
-            Row(
-              children: [
-                Expanded(
-                  child: Divider(
-                    thickness: 1.5,
-                    color: Colors.grey[250],
-                    endIndent: 10,
-                  ),
-                ),
-                const Text('or', style: TextStyle(fontSize: 16)),
-                Expanded(
-                  child: Divider(
-                    thickness: 1.5,
-                    color: Colors.grey[250],
-                    indent: 10,
-                  ),
-                ),
-              ],
-            ),
-            const SizedBox(height: 10),
-            Row(
-              children: [
-                Container(
-                  width: 40,
-                  height: 40,
-                  margin: const EdgeInsets.only(left: 140),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(12),
-                    color: Colors.grey[200],
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.black.withOpacity(0.1),
-                        spreadRadius: 2,
-                        blurRadius: 5,
-                        offset: Offset(0, 3),
-                      ),
-                    ],
-                  ),
-                  child: Image.asset('asset/icons/gicon.png'),
-                ),
-                Container(
-                  width: 40,
-                  height: 40,
-                  margin: const EdgeInsets.only(left: 25),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(12),
-                    color: Colors.grey[200],
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.black.withOpacity(0.1),
-                        spreadRadius: 2,
-                        blurRadius: 5,
-                        offset: Offset(0, 3),
-                      ),
-                    ],
-                  ),
-                  child: Image.asset('asset/icons/Apple-Logo.png'),
-                ),
-              ],
-            ),
-            const SizedBox(height: 35),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text(
-                  "Already have an account?",
-                  style: TextStyle(fontSize: 16),
-                ),
-                TextButton(
+              const SizedBox(height: 40),
+              Padding(
+                padding: const EdgeInsets.only(left: 20),
+                child: ElevatedButton(
                   onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => const Loginview(),
-                      ),
-                    );
+                    // Handle login action
                   },
-                  child: const Text(
-                    'Login',
-                    style: TextStyle(
-                      fontSize: 16,
-                      color: Colors.blue,
-                      fontWeight: FontWeight.bold,
-                      decoration: TextDecoration.underline,
+                  style: ElevatedButton.styleFrom(
+                    fixedSize: const Size(340, 50),
+                    backgroundColor: const Color(0xFF4A90E2),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(24),
                     ),
                   ),
+                  child: const Text(
+                    'Register',
+                    style: TextStyle(fontSize: 16, color: Colors.white),
+                  ),
                 ),
-              ],
-            ),
-          ],
+              ),
+              const SizedBox(height: 20),
+              Row(
+                children: [
+                  Expanded(
+                    child: Divider(
+                      thickness: 1.5,
+                      color: Colors.grey[250],
+                      endIndent: 10,
+                    ),
+                  ),
+                  const Text('or', style: TextStyle(fontSize: 16)),
+                  Expanded(
+                    child: Divider(
+                      thickness: 1.5,
+                      color: Colors.grey[250],
+                      indent: 10,
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 10),
+              Row(
+                children: [
+                  Container(
+                    width: 40,
+                    height: 40,
+                    margin: const EdgeInsets.only(left: 140),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(12),
+                      color: Colors.grey[200],
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black.withOpacity(0.1),
+                          spreadRadius: 2,
+                          blurRadius: 5,
+                          offset: Offset(0, 3),
+                        ),
+                      ],
+                    ),
+                    child: Image.asset('asset/icons/gicon.png'),
+                  ),
+                  Container(
+                    width: 40,
+                    height: 40,
+                    margin: const EdgeInsets.only(left: 25),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(12),
+                      color: Colors.grey[200],
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black.withOpacity(0.1),
+                          spreadRadius: 2,
+                          blurRadius: 5,
+                          offset: Offset(0, 3),
+                        ),
+                      ],
+                    ),
+                    child: Image.asset('asset/icons/Apple-Logo.png'),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 35),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    "Already have an account?",
+                    style: TextStyle(fontSize: 16),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const Loginview(),
+                        ),
+                      );
+                    },
+                    child: const Text(
+                      'Login',
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Colors.blue,
+                        fontWeight: FontWeight.bold,
+                        decoration: TextDecoration.underline,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
